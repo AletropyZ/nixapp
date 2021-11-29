b20dde9da224b1688cacee543f2be5d49bde8596
Davi POrtela <aletropyhq@gmail.com>
2021-11-29 00:32:02 -0300

    GIANT Update

--- public/js/home.js
+++ public/js/home.js
@@ -3,8 +3,6 @@ var stats = {};
 
 LoadUserInfo(); //Carrega as informações de usuário na primeira vez que entrar
 
-
-
 function LoadUserInfo()
 {
     socket.emit('GetUserInfo', USERNAME);
@@ -38,3 +36,48 @@ socket.on('LoadUserInfo', (STATS) => {
     stats = STATS;
     UpdateStats();
 });
+
+//Função para mostrar a tabela de editar os status do personagem
+function ShowEditStats(stat, value)
+{
+    if(stat == 0 && value == 1)
+    {
+        document.getElementById('vida-edit').style.left = 0;
+        document.getElementById('edit-vida-value').value = stats.vida;
+        document.getElementById('edit-vida-max-value').value = stats.maxVida;
+    }
+    else if(stat == 0 && value == 0)
+    {
+        document.getElementById('vida-edit').style.left = '-250px';
+    }
+    else if(stat == 1 && value == 1)
+    {
+        document.getElementById('umbra-edit').style.left = 0;
+        document.getElementById('edit-umbra-value').value = stats.umbra;
+    }
+    else if(stat == 1 && value == 0)
+    {
+        document.getElementById('umbra-edit').style.left = '-250px';
+    }
+}
+
+//Aplica as edições feitas nos status
+function EditStat(stat)
+{
+    if(stat == 0)
+    {
+        let value = document.getElementById('edit-vida-value').value;
+        let maxValue = document.getElementById('edit-vida-max-value').value;
+        stats.vida = value;
+        stats.maxVida = maxValue;
+        document.getElementById('vida-edit').style.left = '-250px';
+    }
+    else if(stat == 1)
+    {
+        let value = document.getElementById('edit-umbra-value').value;
+        stats.umbra = value;
+        document.getElementById('umbra-edit').style.left = '-250px';
+    }
+
+    socket.emit('UpdateUserStats', USERNAME, stats);
+}
